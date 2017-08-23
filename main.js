f63811227e23361c1c8da2b2f4d9562a7be7f43d
Kelly Stoops <kstoops815@gmail.com>
2017-08-22 20:27:10 -0500

    added cats.json file

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-var cats = [
-	{name: "Fluffy", color: "Green", url:"http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg"},
-	{name: "Killer", color: "White", url:"https://ichef.bbci.co.uk/news/660/cpsprodpb/0452/production/_96360110_crabbycat.jpg"},
-	{name: "Sabrina", color: "Black", url:"https://www.petfinder.com/wp-content/uploads/2013/09/cat-black-superstitious-fcs-cat-myths-162286659.jpg"}
-];
+
 
 
 
