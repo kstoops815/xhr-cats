d8718a928a434239901eecc5d4675e146e429fc4
Kelly Stoops <kstoops815@gmail.com>
2017-08-22 21:17:12 -0500

    lesson on json

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-
-
-
-
 function domString(getCat){
 	var catString = ""
 	for(var i = 0; i < getCat.length; i++){
@@ -22,4 +18,24 @@ function writeToDom(shit){
 
 
 
-domString(cats);
+//domString(cats);
+function executeThisCodeAfterFileLoads(){
+	var data = JSON.parse(this.responseText);
+	domString(data.cats);
+}
+
+function executeThisCodeIfFileErrors(){
+	console.log("Shit broke");
+}
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoads);
+myRequest.addEventListener("error", executeThisCodeIfFileErrors);
+myRequest.open("GET", "cats.json")
+myRequest.send();
+
+var myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener("load", executeThisCodeAfterFileLoads);
+myRequest2.addEventListener("error", executeThisCodeIfFileErrors);
+myRequest2.open("GET", "dogs.json");
+myRequest2.send();
